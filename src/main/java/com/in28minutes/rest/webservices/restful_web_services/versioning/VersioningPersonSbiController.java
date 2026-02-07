1edd5d68e9eff993c3b3f4ab3214be35882aa738
Anuj Gupta <apgupta3091@gmail.com>
2026-02-07 14:27:48 -0500

    use spring versioning

---
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/versioning/VersioningPersonSbiController.java
@@ -0,0 +1,29 @@
+package com.in28minutes.rest.webservices.restful_web_services.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonSbiController {
+
+//    @GetMapping(value = "/{version}/personsbi", version = "1.0.0")
+//    public PersonV1 getFirstVersionOfPerson() {
+//        return new PersonV1("Bob Charlie");
+//    }
+//
+//    @GetMapping(value = "/{version}/personsbi", version = "2.0.0")
+//    public PersonV2 getSecondVersionOfPerson() {
+//        return new PersonV2(new Name("Bob", "Charlie"));
+//    }
+
+    @GetMapping(path = "/personsbi", version = "1.0.0")
+    public PersonV1 getFirstVersionOfPersonRequestParam() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/personsbi", version = "2.0.0")
+    public PersonV2 getSecondVersionOfPersonRequestParam() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+}

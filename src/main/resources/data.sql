19b71e4eaae75494610065370b68047ec4170117
Anuj Gupta <apgupta3091@gmail.com>
2026-02-09 18:41:20 -0500

    hi

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user_details (birth_date, id, name)
+VALUES(current_date(), 10001, 'ranga');
+
+INSERT INTO user_details (birth_date, id, name)
+VALUES(current_date(), 10002, 'anuj');
+
+INSERT INTO user_details (birth_date, id, name)
+VALUES(current_date(), 10003, 'alexa');
